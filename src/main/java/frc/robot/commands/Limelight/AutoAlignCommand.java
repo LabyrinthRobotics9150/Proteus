d6222bf8f00632db8e87e7bfa8b7892268766406
Robert <robertaburdsall@gmail.com>
2025-03-06 09:52:06 -0500

    updated

--- src/main/java/frc/robot/commands/Limelight/AutoAlignCommand.java
+++ src/main/java/frc/robot/commands/Limelight/AutoAlignCommand.java
@@ -5,19 +5,19 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.LimelightHelpers.RawFiducial;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 
 class PIDControllerConfigurable extends PIDController {
     public PIDControllerConfigurable(double kP, double kI, double kD) {
         super(kP, kI, kD);
     }
-
+    
     public PIDControllerConfigurable(double kP, double kI, double kD, double tolerance) {
         super(kP, kI, kD);
         this.setTolerance(tolerance);
@@ -35,7 +35,7 @@ public class AutoAlignCommand extends Command {
     private static final SwerveRequest.RobotCentric alignRequest = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
     private static final SwerveRequest.Idle idleRequest = new SwerveRequest.Idle();
     private static int tagID = -1;
-
+    
     public double rotationalRate = 0;
     public double velocityX = 0;
     public double velocityY = 0;
@@ -46,12 +46,10 @@ public class AutoAlignCommand extends Command {
         addRequirements(m_Limelight);
     }
 
-    public AutoAlignCommand(CommandSwerveDrivetrain drivetrain, VisionSubsystem limelight, int ID) throws IllegalArgumentException {
+    public AutoAlignCommand(CommandSwerveDrivetrain drivetrain, VisionSubsystem limelight, int ID) {
         this.m_drivetrain = drivetrain;
         this.m_Limelight = limelight;
-        if (ID < 0) {
-            throw new IllegalArgumentException("april tag id cannot be negative");
-        }
+        if (ID < 0) throw new IllegalArgumentException("AprilTag ID cannot be negative");
         tagID = ID;
         addRequirements(m_Limelight);
     }
@@ -61,48 +59,63 @@ public class AutoAlignCommand extends Command {
 
     @Override
     public void execute() {
-        rotationalPidController = new PIDControllerConfigurable(SmartDashboard.getNumber("Rotate P", 0.0), VisionConstants.ROTATE_I, SmartDashboard.getNumber("Rotate D", 0.0), VisionConstants.TOLERANCE);
+        rotationalPidController = new PIDControllerConfigurable(
+            SmartDashboard.getNumber("Rotate P", 1), 
+            VisionConstants.ROTATE_I, 
+            SmartDashboard.getNumber("Rotate D", 0.0), 
+            VisionConstants.TOLERANCE
+        );
 
-        RawFiducial fiducial;
         try {
+            RawFiducial fiducial;
             if (tagID == -1) {
                 fiducial = m_Limelight.getFiducialWithId(m_Limelight.getClosestFiducial().id);
             } else {
                 fiducial = m_Limelight.getFiducialWithId(tagID);
             }
 
-            rotationalRate = rotationalPidController.calculate(2 * fiducial.txnc, 0.0) * RotationsPerSecond.of(0.75).in(RadiansPerSecond) * -0.1;
-            this.velocityX = xPidController.calculate(fiducial.distToRobot, 0.3) * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.6;
-            this.velocityY = yPidController.calculate(fiducial.tync, 0.0) * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.6;
+            // Calculate rotation control
+            rotationalRate = rotationalPidController.calculate(fiducial.txnc, 0.0) 
+                * RotationsPerSecond.of(0.75).in(RadiansPerSecond) 
+                * -0.1;
 
-            if (rotationalPidController.atSetpoint() && xPidController.atSetpoint() && yPidController.atSetpoint()) {
-                this.end(false);
-            }
+            // Calculate X velocity (forward/backward)
+            velocityX = xPidController.calculate(fiducial.distToRobot, 1.0) 
+                * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) 
+                * 0.6;
 
+            // Calculate Y velocity (strafing)
+            double yError = fiducial.distToRobot * Math.sin(Units.degreesToRadians(fiducial.txnc));
+            velocityY = yPidController.calculate(yError, 0.0) 
+                * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) 
+                * 0.6;
+
+            // Apply movement
+            m_drivetrain.setControl(
+                alignRequest
+                    .withRotationalRate(-rotationalRate)
+                    .withVelocityX(-velocityX)
+                    .withVelocityY(velocityY)
+            );
+
+            // Update dashboard
             SmartDashboard.putNumber("txnc", fiducial.txnc);
             SmartDashboard.putNumber("distToRobot", fiducial.distToRobot);
-            SmartDashboard.putNumber("yError", fiducial.tync);
-            SmartDashboard.putNumber("rotationalPidController", rotationalRate);
-            SmartDashboard.putNumber("xPidController", this.velocityX);
-            SmartDashboard.putNumber("yPidController", this.velocityY);
+            SmartDashboard.putNumber("rotationalRate", rotationalRate);
+            SmartDashboard.putNumber("velocityX", velocityX);
+            SmartDashboard.putNumber("velocityY", velocityY);
 
-            m_drivetrain.setControl(
-                    alignRequest.withRotationalRate(-rotationalRate)
-                            .withVelocityX(-this.velocityX)
-                            .withVelocityY(this.velocityY));
-
-        } catch (VisionSubsystem.NoSuchTargetException nste) {
-            System.out.println("No apriltag found");
-            m_drivetrain.setControl(
-                    alignRequest.withRotationalRate(-1.5 * rotationalRate)
-                            .withVelocityX(-1.5 * this.velocityX)
-                            .withVelocityY(1.5 * this.velocityY));
+        } catch (VisionSubsystem.NoSuchTargetException e) {
+            System.out.println("No AprilTag found");
+            m_drivetrain.setControl(idleRequest);
         }
     }
 
     @Override
     public boolean isFinished() {
-        return rotationalPidController.atSetpoint() && xPidController.atSetpoint() && yPidController.atSetpoint();
+        return rotationalPidController.atSetpoint() 
+            && xPidController.atSetpoint() 
+            && yPidController.atSetpoint();
     }
 
     @Override
