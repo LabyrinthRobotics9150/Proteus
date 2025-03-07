a79a2eb4feeff240cd3a67567df8355d86989570
Robert <robertaburdsall@gmail.com>
2025-03-06 22:49:47 -0500

    upgraded wpilib

---
+++ src/main/java/frc/robot/commands/autos/ShootCommand.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class ShootCommand extends Command {
+    IntakeSubsystem intake;
+    double speed;
+    public ShootCommand(IntakeSubsystem intake, double speed) {
+        this.intake = intake;
+        this.speed = speed;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        intake.moveWheel(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intake.stopWheel(); 
+    }
+
+}
