96e6e7666b50a5d54d67f1e9418b527cf8c76b9f
robertburdsall <robertaburdsall@gmail.com>
2025-03-13 17:09:48 -0400

    new test autoalign

--- src/main/java/frc/robot/commands/Limelight/AlignToReefTagRelative.java
+++ src/main/java/frc/robot/commands/Limelight/AlignToReefTagRelative.java
@@ -7,11 +7,9 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.LimelightHelpers;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
